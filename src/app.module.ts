63ff279fe9046e5d1e5217f502addaf1af7ee304
jamborghini <tyserract@gmail.com>
2021-07-27 03:49:17 +0300

    fear: add microservice modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppTypeormModule } from 'src/module/misc/app-typeorm/app-typeorm.module';
+import { AppConfigModule } from 'src/module/misc/app-config/app-config.module';
+import { UserModule } from 'src/module/user/user.module';
+import { AuthModule } from 'src/module/auth/auth.module';
 
-@Module({})
+@Module({
+  imports: [AppTypeormModule, AppConfigModule, UserModule, AuthModule],
+})
 export class AppModule {}
