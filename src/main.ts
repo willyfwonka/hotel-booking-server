fbb97f87c5868d88c1d4524a9c5b5a1ab619d6f9
jamborghini <tyserract@gmail.com>
2021-07-27 17:21:20 +0300

    refactor: add helmet, see: https://github.com/helmetjs/helmet

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({
-    origin: 'http://localhost:4200', // Port value of a default Angular Project
-    credentials: true,
-  });
+  app.use(helmet());
 
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(
