db680f10661ddf84b4923948d2e2050bca526727
jamborghini <tyserract@gmail.com>
2021-07-29 22:47:51 +0300

    fix: could not compare string and bigint

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -6,7 +6,6 @@ import {
   Post,
   Req,
   UnauthorizedException,
-  UseInterceptors,
 } from '@nestjs/common';
 import { User } from 'src/module/user/model/user';
 import { UpdateUser } from 'src/module/user/input/update-user';
@@ -14,7 +13,6 @@ import { CreateUser } from 'src/module/user/input/create-user';
 import { Id } from 'src/module/shared/decorator/param/id';
 import { RegisterProducerService } from 'src/module/misc/app-queue/service/register-producer.service';
 import { RateLimit } from 'src/module/auth/guard/rate-limit';
-import { GetUserInterceptor } from 'src/module/interceptor/get-user-interceptor';
 import { Authorize } from 'src/module/auth/decorator/authorize';
 
 @Controller('user')
@@ -27,9 +25,8 @@ export class UserController {
     return this.registerService.addToRegisterQueue(body);
   }
 
-  @RateLimit(1, 10)
   @Authorize()
-  @UseInterceptors(GetUserInterceptor)
+  @RateLimit(1, 10)
   @Patch(':id')
   async updateUser(
     @Req() req,
@@ -38,21 +35,20 @@ export class UserController {
   ): Promise<User> {
     // Checking if user is trying to update another user
     // If user is not updating own, throw an http exception
-    if (id !== req?.user?.id) {
+    if (BigInt(id).toString() !== req?.user?.id) {
       throw new UnauthorizedException('Missing permissions');
     }
 
     return User.findOneAndUpdate({ id, ...body });
   }
 
-  @RateLimit(1, 10)
   @Authorize()
-  @UseInterceptors(GetUserInterceptor)
+  @RateLimit(1, 10)
   @Delete(':id')
   async deleteUser(@Req() req, @Id() id: bigint): Promise<User> {
     // Checking if user is trying to delete another user
     // If user is not deleting own, throw an http exception
-    if (id !== req?.user?.id) {
+    if (BigInt(id).toString() !== req?.user?.id) {
       throw new UnauthorizedException('Missing permissions');
     }
 
