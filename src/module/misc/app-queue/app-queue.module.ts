a9a23a35135aaff763cdcc25d36654c27922cf6a
jamborghini <tyserract@gmail.com>
2021-07-29 22:27:48 +0300

    fix: redis could not process

--- src/module/misc/app-queue/app-queue.module.ts
+++ src/module/misc/app-queue/app-queue.module.ts
@@ -18,10 +18,6 @@ import { ReservationConsumer } from 'src/module/misc/app-queue/consumer/reservat
         configService: ConfigService,
       ): Promise<QueueOptions> => ({
         redis: {
-          // Reconnecting to redis on error
-          reconnectOnError() {
-            return 2;
-          },
           host: configService.get('STORE_HOST'),
           port: configService.get('STORE_PORT'),
           db: configService.get('STORE_QUEUE_DB'),
