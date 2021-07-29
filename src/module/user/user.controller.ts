164896869d6766deaf1a3451ba8f52f726ffc88e
jamborghini <tyserract@gmail.com>
2021-07-29 22:37:31 +0300

    fix: users could update/delete other users

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Delete, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Patch,
+  Post,
+  Req,
+  UnauthorizedException,
+  UseInterceptors,
+} from '@nestjs/common';
 import { User } from 'src/module/user/model/user';
 import { UpdateUser } from 'src/module/user/input/update-user';
 import { CreateUser } from 'src/module/user/input/create-user';
 import { Id } from 'src/module/shared/decorator/param/id';
 import { RegisterProducerService } from 'src/module/misc/app-queue/service/register-producer.service';
 import { RateLimit } from 'src/module/auth/guard/rate-limit';
+import { GetUserInterceptor } from 'src/module/interceptor/get-user-interceptor';
+import { Authorize } from 'src/module/auth/decorator/authorize';
 
 @Controller('user')
 export class UserController {
@@ -17,14 +28,34 @@ export class UserController {
   }
 
   @RateLimit(1, 10)
+  @Authorize()
+  @UseInterceptors(GetUserInterceptor)
   @Patch(':id')
-  async updateUser(@Body() body: UpdateUser, @Id() id: bigint): Promise<User> {
+  async updateUser(
+    @Req() req,
+    @Body() body: UpdateUser,
+    @Id() id: bigint,
+  ): Promise<User> {
+    // Checking if user is trying to update another user
+    // If user is not updating own, throw an http exception
+    if (id !== req?.user?.id) {
+      throw new UnauthorizedException('Missing permissions');
+    }
+
     return User.findOneAndUpdate({ id, ...body });
   }
 
   @RateLimit(1, 10)
+  @Authorize()
+  @UseInterceptors(GetUserInterceptor)
   @Delete(':id')
-  async deleteUser(@Id() id: bigint): Promise<User> {
+  async deleteUser(@Req() req, @Id() id: bigint): Promise<User> {
+    // Checking if user is trying to delete another user
+    // If user is not deleting own, throw an http exception
+    if (id !== req?.user?.id) {
+      throw new UnauthorizedException('Missing permissions');
+    }
+
     const user = await User.findOneOrFail({ id });
     return user.softRemove();
   }
--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { AppQueueModule } from 'src/module/misc/app-queue/app-queue.module';
+import { AuthModule } from 'src/module/auth/auth.module';
 
 @Module({
-  imports: [AppQueueModule],
+  imports: [AppQueueModule, AuthModule],
   controllers: [UserController],
 })
 export class UserModule {}
